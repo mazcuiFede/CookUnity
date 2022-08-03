2ffc2626b4ec8622850b65c03e08c92a4f4f2672
Matias Azcui <matias.azcui@gmail.com>
2022-08-03 18:01:04 +0200

    Clean code improvements

--- index.js
+++ index.js
@@ -13,34 +13,32 @@ const findPath = (solution) => {
     const neighbours = getNeighbours(maze, solution, root)
     const exit = findExit(maze, neighbours, solution)
 
-    console.clear()
+    if (exit.length === 0) {
 
-    if (exit.length > 0) {
+      var possibleNeighbours = neighbours.filter(neighbour => shouldAddValue(maze, solution, neighbour))
+
+      if (possibleNeighbours.length === 0) {
+        const last = solution.pop()
+        maze[last[0]][last[1]] = -1
+        findPath(solution)
+      }
+      
+      possibleNeighbours.map(newRoot => {
+        solution.push(newRoot)
+        findPath(solution)
+      })
+
+    }
+    else {
       solution.push(exit[0])
+
       solution.map((x) => {
         console.log(positionToChar(maze, x), "at position", x)
       })
+
       console.log('End')
-    } else {
-      if (neighbours[0] && shouldAddValue(maze, solution, neighbours[0])) {
-        solution.push(neighbours[0])
-        findPath(solution)
-      } else {
-        if (neighbours[1] && shouldAddValue(maze, solution, neighbours[1])) {
-          solution.push(neighbours[1])
-          findPath(solution)
-        } else {
-          if (neighbours[2] && shouldAddValue(maze, solution, neighbours[2])) {
-            solution.push(neighbours[2])
-            findPath(solution)
-          } else {
-            const last = solution.pop()
-            maze[last[0]][last[1]] = -1
-            findPath(solution)
-          }
-        }
-      }
     }
+
   }
 
 }
